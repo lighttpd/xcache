33df6f8660f87726bdbd0d9a4bc7d019cd6c9bd6
moo <moo>
2006-09-04 12:53:42 +0000

    Chinese Traditional translation. thanks to Finjon Kiang.
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@135 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

---
+++ admin/common-zh-traditional-utf-8.lang.php
@@ -0,0 +1,102 @@
+<?php
+
+$strings = array(
+		'Cache Legends'
+		=> 'Cache 說明',
+		'List Legends'
+		=> 'List 說明',
+		'XCache Administration Help'
+		=> 'XCache 管理頁面說明訊息',
+		'Help'
+		=> '說明',
+		'Slots'
+		=> '槽',
+		'Size'
+		=> '大小',
+		'Avail'
+		=> '剩餘',
+		'Used'
+		=> '已用',
+		'Clear'
+		=> '清除',
+		'Sure to clear?'
+		=> '確認要清除嗎?',
+		'Compiling'
+		=> '編譯中',
+		'%'
+		=> '%',
+		'Hits'
+		=> '命中',
+		'Misses'
+		=> '錯過',
+		'Clogs'
+		=> '阻塞',
+		'OOMs'
+		=> '記憶體不足',
+		'Protected'
+		=> '保護',
+		'Cached'
+		=> '快取',
+		'Deleted'
+		=> '待刪',
+		'Delete'
+		=> '刪除',
+		'Free Blocks'
+		=> '空閒塊',
+
+		'entry'
+		=> '項目',
+		'Refcount'
+		=> '引用數',
+		'SrcSize'
+		=> '原始檔案大小',
+		'Modify'
+		=> '修改',
+		'device'
+		=> '設備',
+		'inode'
+		=> 'Inode',
+		'Access'
+		=> '存取',
+		'Create'
+		=> '建立',
+		'See also'
+		=> '建議參考',
+		'GC'
+		=> 'GC',
+		'Cache'
+		=> '快取',
+		'Caches'
+		=> '快取',
+		'Cached'
+		=> '快取',
+		'php Cached'
+		=> '快取的 php 指令',
+		'php Deleted'
+		=> '待刪 php 指令',
+		'var Cached'
+		=> '快取的變數',
+		'var Deleted'
+		=> '待刪變數',
+		'Statistics'
+		=> '摘要統計',
+		'List PHP'
+		=> '列出PHP',
+		'List Var Data'
+		=> '列變數資料',
+		'XCache %s Administration'
+		=> 'XCache %s 管理頁面',
+		'size'
+		=> '大小',
+		'offset'
+		=> '位置',
+		''
+		=> '',
+		''
+		=> '',
+		''
+		=> '',
+		''
+		=> '',
+		);
+
