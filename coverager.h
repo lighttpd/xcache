5230b6047ffa71294cab849a16169847164390bc
Xuefer <xuefer@gmail.com>
2006-10-01 08:57:51 +0000

    coverager: new api xcache_coverager_start/xcache_coverager_stop/xcache_coverager_get(), coverager ini changed
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@204 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- coverager.h
+++ coverager.h
@@ -1,11 +1,12 @@
 #include "php.h"
 #include "xcache.h"
 
-extern char *xc_coveragedump_dir;
-
 void xc_coverager_handle_ext_stmt(zend_op_array *op_array, zend_uchar op);
 int xc_coverager_init(int module_number TSRMLS_DC);
 void xc_coverager_destroy();
 void xc_coverager_request_init(TSRMLS_D);
 void xc_coverager_request_shutdown(TSRMLS_D);
 PHP_FUNCTION(xcache_coverager_decode);
+PHP_FUNCTION(xcache_coverager_start);
+PHP_FUNCTION(xcache_coverager_stop);
+PHP_FUNCTION(xcache_coverager_get);
