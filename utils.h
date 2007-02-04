d6e525a5311fe9f7b46914fa2146030416b651e5
moo <moo>
2007-02-04 08:15:00 +0000

    copy function/class table correctly by using add_ref
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@345 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- utils.h
+++ utils.h
@@ -74,5 +74,6 @@ typedef struct {
 	Bucket    *tmp_internal_class_tail;
 } xc_sandbox_t;
 
+void xc_zend_class_add_ref(zend_class_entry ZESW(*ce, **ce));
 xc_sandbox_t *xc_sandbox_init(xc_sandbox_t *sandbox, char *filename TSRMLS_DC);
 void xc_sandbox_free(xc_sandbox_t *sandbox, int install TSRMLS_DC);
