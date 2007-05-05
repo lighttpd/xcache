0339491d77e08d96b4d744f5acc5fd344009ce8e
Xuefer <xuefer@gmail.com>
2007-05-05 10:40:31 +0000

    merged [371] from trunk->1.2: remove/edit variable data in admin page
    git-svn-id: svn://svn.lighttpd.net/xcache/branches/1.2@373 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

---
+++ admin/header.tpl.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php
+echo <<<HEAD
+	<meta http-equiv="Content-Type" content="text/html; charset=$charset" />
+	<meta http-equiv="Content-Language" content="$lang" />
+	<script type="text/javascript" src="tablesort.js" charset="$charset"></script>
+HEAD;
+?>
+
+	<link rel="stylesheet" type="text/css" href="xcache.css" />
+	<title><?php echo sprintf(_T("XCache %s Administration"), $xcache_version); ?></title>
+</head>
+
+<body>
+<h1><?php echo sprintf(_T("XCache %s Administration"), $xcache_version); ?></h1>
