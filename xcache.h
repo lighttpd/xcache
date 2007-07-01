3e413e1f776411cd8bfdd41695fecb0e2dad3693
moo <moo>
2007-07-01 08:54:11 +0000

    bump version and years
    git-svn-id: svn://svn.lighttpd.net/xcache/tags/1.2.1@441 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- xcache.h
+++ xcache.h
@@ -1,9 +1,9 @@
 #ifndef __XCACHE_H
 #define __XCACHE_H
 #define XCACHE_NAME       "XCache"
-#define XCACHE_VERSION    "1.2.1-dev"
+#define XCACHE_VERSION    "1.2.1"
 #define XCACHE_AUTHOR     "mOo"
-#define XCACHE_COPYRIGHT  "Copyright (c) 2005-2006"
+#define XCACHE_COPYRIGHT  "Copyright (c) 2005-2007"
 #define XCACHE_URL        "http://xcache.lighttpd.net"
 
 #include <php.h>
