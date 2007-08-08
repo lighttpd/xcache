d807b0ea32422763c9c5dd3d0aba10260e230088
Xuefer <xuefer@gmail.com>
2007-08-08 16:15:03 +0000

    oops
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@474 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- xcache.c
+++ xcache.c
@@ -830,7 +830,7 @@ not_relative_path:
 		/* fall */
 
 stat_done:
-		delta = XG(request_time) - pbuf->st_mtime < 2;
+		delta = XG(request_time) - pbuf->st_mtime;
 		if (abs(delta) < 2 && !xc_test) {
 			return FAILURE;
 		}
