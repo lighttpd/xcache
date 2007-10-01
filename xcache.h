8e153e57fb440f53d9f83344c4c6aab63e28ea7e
moo <moo>
2007-10-01 11:24:34 +0000

    compile against PHP_5_3
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@480 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- xcache.h
+++ xcache.h
@@ -25,6 +25,9 @@
 #if !defined(ZEND_ENGINE_2_2) && (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION >= 2 || PHP_MAJOR_VERSION > 5)
 #	define ZEND_ENGINE_2_2
 #endif
+#if !defined(ZEND_ENGINE_2_3) && (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION >= 3 || PHP_MAJOR_VERSION > 5)
+#	define ZEND_ENGINE_2_3
+#endif
 
 #define NOTHING
 /* ZendEngine code Switcher */
