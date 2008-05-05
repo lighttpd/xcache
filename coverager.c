a1bab03a43ba76f146964314d20855acbeb379be
Xuefer <xuefer@gmail.com>
2008-05-05 11:50:54 +0000

    fix build for vc for strict c rule
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@554 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- coverager.c
+++ coverager.c
@@ -473,8 +473,9 @@ int xc_coverager_init(int module_number TSRMLS_DC) /* {{{ */
 	zend_compile_file = xc_compile_file_for_coverage;
 
 	if (cfg_get_string("xcache.coveragedump_directory", &xc_coveragedump_dir) == SUCCESS && xc_coveragedump_dir) {
+		int len;
 		xc_coveragedump_dir = pestrdup(xc_coveragedump_dir, 1);
-		int len = strlen(xc_coveragedump_dir);
+		len = strlen(xc_coveragedump_dir);
 		if (len) {
 			if (xc_coveragedump_dir[len - 1] == '/') {
 				xc_coveragedump_dir[len - 1] = '\0';
