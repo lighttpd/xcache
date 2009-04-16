5e65b7913af6e2acdf4165a04d6b0e6875354948
moo <moo>
2009-04-16 08:43:35 +0000

    added $enable_eval
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@591 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- admin/edit.tpl.php
+++ admin/edit.tpl.php
@@ -6,7 +6,7 @@ $h_value = htmlspecialchars($value);
 <form method="post" action="">
 	<fieldset>
 		<legend><?php echo sprintf(_T("Editing Variable %s"), $h_name); ?></legend>
-		<textarea name="value" style="width: 100%; height: 200px; overflow-y: auto"><?php echo $h_value; ?></textarea><br>
+		<textarea name="value" style="width: 100%; height: 200px; overflow-y: auto" <?php echo $editable ? "" : "disabled=disabled"; ?>><?php echo $h_value; ?></textarea><br>
 		<input type="submit">
 	</fieldset>
 </form>
