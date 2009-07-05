ff78ab8a5a378d77d65d76e544e5a803cb3ace46
Xuefer <xuefer@gmail.com>
2009-07-05 14:56:27 +0000

    merged r637,r634,r640 from trunk
    git-svn-id: svn://svn.lighttpd.net/xcache/branches/1.3@641 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- xcache.c
+++ xcache.c
@@ -1584,22 +1584,11 @@ err:
 	return 0;
 }
 /* }}} */
-static void xc_copy_zend_constant(zend_constant *c) /* {{{ */
-{
-	c->name = zend_strndup(c->name, c->name_len - 1);
-	if (!(c->flags & CONST_PERSISTENT)) {
-		zval_copy_ctor(&c->value);
-	}
-}
-/* }}} */
 static void xc_request_init(TSRMLS_D) /* {{{ */
 {
 	int i;
 
 	if (!XG(internal_table_copied)) {
-#ifdef HAVE_XCACHE_CONSTANT
-		zend_constant tmp_const;
-#endif
 		zend_function tmp_func;
 		xc_cest_t tmp_cest;
 
@@ -1610,13 +1599,13 @@ static void xc_request_init(TSRMLS_D) /* {{{ */
 		zend_hash_destroy(&XG(internal_class_table));
 
 #ifdef HAVE_XCACHE_CONSTANT
-		zend_hash_init_ex(&XG(internal_constant_table), 20,  NULL, NULL, 1, 0);
+		zend_hash_init_ex(&XG(internal_constant_table), 20,  NULL, (dtor_func_t) xc_zend_constant_dtor, 1, 0);
 #endif
 		zend_hash_init_ex(&XG(internal_function_table), 100, NULL, NULL, 1, 0);
 		zend_hash_init_ex(&XG(internal_class_table),    10,  NULL, NULL, 1, 0);
 
 #ifdef HAVE_XCACHE_CONSTANT
-		zend_hash_copy(&XG(internal_constant_table), EG(zend_constants), (copy_ctor_func_t) xc_copy_zend_constant, &tmp_const, sizeof(tmp_const));
+		xc_copy_internal_zend_constants(&XG(internal_constant_table), EG(zend_constants));
 #endif
 		zend_hash_copy(&XG(internal_function_table), CG(function_table), NULL, &tmp_func, sizeof(tmp_func));
 		zend_hash_copy(&XG(internal_class_table), CG(class_table), NULL, &tmp_cest, sizeof(tmp_cest));
@@ -1677,7 +1666,7 @@ void xc_init_globals(zend_xcache_globals* xcache_globals TSRMLS_DC)
 	memset(xcache_globals, 0, sizeof(zend_xcache_globals));
 
 #ifdef HAVE_XCACHE_CONSTANT
-	zend_hash_init_ex(&xcache_globals->internal_constant_table, 1, NULL, NULL, 1, 0);
+	zend_hash_init_ex(&xcache_globals->internal_constant_table, 1, NULL, (dtor_func_t) xc_zend_constant_dtor, 1, 0);
 #endif
 	zend_hash_init_ex(&xcache_globals->internal_function_table, 1, NULL, NULL, 1, 0);
 	zend_hash_init_ex(&xcache_globals->internal_class_table,    1, NULL, NULL, 1, 0);
@@ -1957,7 +1946,7 @@ static int xc_entry_init_key_var(xc_entry_t *xce, zval *name TSRMLS_DC) /* {{{ *
 			break;
 		default:
 #ifdef IS_UNICODE
-			convert_to_text(name);
+			convert_to_unicode(name);
 #else
 			convert_to_string(name);
 #endif
