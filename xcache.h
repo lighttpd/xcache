f4b9b37f4a37446f64db776bf86d538e90523475
Xuefer <xuefer@gmail.com>
2010-11-27 14:16:46 +0000

    bump version to 1.3.1
    git-svn-id: svn://svn.lighttpd.net/xcache/branches/1.3@692 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- xcache.h
+++ xcache.h
@@ -1,9 +1,9 @@
 #ifndef __XCACHE_H
 #define __XCACHE_H
 #define XCACHE_NAME       "XCache"
-#define XCACHE_VERSION    "1.3.0"
+#define XCACHE_VERSION    "1.3.1"
 #define XCACHE_AUTHOR     "mOo"
-#define XCACHE_COPYRIGHT  "Copyright (c) 2005-2009"
+#define XCACHE_COPYRIGHT  "Copyright (c) 2005-2010"
 #define XCACHE_URL        "http://xcache.lighttpd.net"
 #define XCACHE_WIKI_URL   XCACHE_URL "/wiki"
 
