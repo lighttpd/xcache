50c0bf59a63ccd61d2484f93d658a4d071848745
moo <moo>
2012-06-28 03:45:14 +0000

    show php version
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@934 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- admin/footer.tpl.php
+++ admin/footer.tpl.php
@@ -1,6 +1,6 @@
 <div class="footnote">
 <?php echo <<<EOS
-Powered By: XCache {$xcache_version}, {$xcache_modules}
+PHP {$php_version} Powered By: XCache {$xcache_version}, {$xcache_modules}
 EOS;
 ?>
 </div>
