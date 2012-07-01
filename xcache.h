939490747984279452f8b8b859cccd51521bdfb8
Xuefer <xuefer@gmail.com>
2012-07-01 04:15:48 +0000

    marks rc2
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@946 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- xcache.h
+++ xcache.h
@@ -2,7 +2,7 @@
 #define __XCACHE_H
 #define XCACHE_NAME       "XCache"
 #ifndef XCACHE_VERSION
-#	define XCACHE_VERSION "2.0.1-dev"
+#	define XCACHE_VERSION "2.0.1-rc2"
 #endif
 #define XCACHE_AUTHOR     "mOo"
 #define XCACHE_COPYRIGHT  "Copyright (c) 2005-2012"
