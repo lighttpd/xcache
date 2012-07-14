59c5b1edbfac7a965fd752c23ca050671f34d255
moo <moo>
2012-07-14 01:30:02 +0000

    bump version to 2.1.0
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@967 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- xcache.h
+++ xcache.h
@@ -2,7 +2,7 @@
 #define __XCACHE_H
 #define XCACHE_NAME       "XCache"
 #ifndef XCACHE_VERSION
-#	define XCACHE_VERSION "2.0.1-rc3"
+#	define XCACHE_VERSION "2.1.0"
 #endif
 #define XCACHE_AUTHOR     "mOo"
 #define XCACHE_COPYRIGHT  "Copyright (c) 2005-2012"
