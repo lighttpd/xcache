388b3cffb539887b600eb206d79aa281a78263f0
Xuefer <xuefer@gmail.com>
2012-07-29 05:34:37 +0000

    htdocs: adds width/height for logo to take up space before loaded
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@1075 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- htdocs/common/header.tpl.php
+++ htdocs/common/header.tpl.php
@@ -14,7 +14,7 @@
 <body>
 <div id="header">
 	<div id="banner">
-		<a href="http://xcache.lighttpd.net/" target="_blank"><img src="../common/xcache.png" id="logo" />
+		<a href="http://xcache.lighttpd.net/" target="_blank"><img src="../common/xcache.png" id="logo" width="175" height="51" />
 			<?php echo $xcache_version; ?> - <?php echo ucfirst($GLOBALS['module']); ?>
 		</a>
 	</div>
