5782061bc8f24592081d683c244928a1c0c59be1
Xuefer <xuefer@gmail.com>
2012-07-30 06:57:52 +0000

    htdocs/cacher: fix hits graph for IE
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@1085 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- htdocs/common/header.tpl.php
+++ htdocs/common/header.tpl.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=6" />
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $GLOBALS['config']['charset']; ?>" />
 	<meta http-equiv="Content-Language" content="<?php echo $GLOBALS['config']['lang']; ?>" />
 	<script type="text/javascript" src="tablesort.js"></script>
