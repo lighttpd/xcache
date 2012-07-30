c23d48c42b33b5ff27814763e6318e264918ef1d
moo <moo>
2012-07-30 07:56:32 +0000

    htdocs: removed help
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@1088 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- htdocs/cacher/help-zh-traditional.lang.php
+++
@@ -1,4 +0,0 @@
-<dl>
-<dt>编译错误: </dt> <dd><a href="http://www.php.net/manual/en/ref.errorfunc.php#ini.error-log">ini.error-log</a> and <a href="http://cn2.php.net/manual/en/ref.errorfunc.php#ini.display-errors">ini.display-errors</a></dd>
-<dt>只读保护: </dt> <dd><a href="http://xcache.lighttpd.net/wiki/ReadonlyProtection">ReadonlyProtection</a></dd>
-</dl>
