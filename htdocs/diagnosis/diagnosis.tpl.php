904183b571161dd044e01f8d2fdae15055558a88
moo <moo>
2012-07-31 09:22:10 +0000

    diagnosis: impl checks
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@1092 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- htdocs/diagnosis/diagnosis.tpl.php
+++ htdocs/diagnosis/diagnosis.tpl.php
@@ -1,24 +1,28 @@
 <?php include "../common/header.tpl.php"; ?>
-<table cellspacing="0" cellpadding="4" class="cycles">
+<table cellspacing="0" cellpadding="4" class="cycles" id="diagnosisResult">
 	<caption>
 		<?php echo _T("Diagnosis Result"); ?>
 	</caption>
 	<tr>
 		<th>
+			<?php echo _T("Item"); ?>
+		</th>
+		<th>
 			<?php echo _T("Level"); ?>
 		</th>
 		<th>
-			<?php echo _T("Reason"); ?>
+			<?php echo _T("Result"); ?>
 		</th>
 		<th>
-			<?php echo _T("Suggestion"); ?>
+			<?php echo _T("Result/Suggestion"); ?>
 		</th>
 	</tr>
 <?php foreach ($notes as $note) { ?>
-	<tr>
-		<td><?php echo ucfirst($note['type']); ?></td>
-		<td><?php echo $note['reason']; ?></td>
-		<td><?php echo $note['suggestion']; ?></td>
+	<tr class="<?php echo $note['type']; ?>">
+		<td nowrap="nowrap" align="right"><?php echo $note['item']; ?></td>
+		<td nowrap="nowrap"><?php echo ucfirst($note['type']); ?></td>
+		<td nowrap="nowrap"><?php echo nl2br($note['result']); ?></td>
+		<td><?php echo nl2br($note['suggestion']); ?></td>
 	</tr>
 <?php } ?>
 </table>
