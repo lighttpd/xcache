0fce15627fc21f9012d09489c0c9f9d952a90f9c
moo <moo>
2012-12-17 10:18:22 +0000

    fix for ZTS
    git-svn-id: svn://svn.lighttpd.net/xcache/trunk@1199 c26eb9a1-5813-0410-bd6c-c2e55f420ca7

--- xcache/xc_lock.h
+++ xcache/xc_lock.h
@@ -7,14 +7,9 @@
 
 typedef struct _xc_lock_t xc_lock_t;
 
-xc_lock_t *xc_fcntl_init(const char *pathname);
-void xc_fcntl_destroy(xc_lock_t *lck);
-void xc_fcntl_lock(xc_lock_t *lck);
-void xc_fcntl_unlock(xc_lock_t *lck);
-
-#define xc_lock_init(name)  xc_fcntl_init(name)
-#define xc_lock_destroy(fd) xc_fcntl_destroy(fd)
-#define xc_lock(fd)         xc_fcntl_lock(fd)
-#define xc_unlock(fd)       xc_fcntl_unlock(fd)
+xc_lock_t *xc_lock_init(const char *pathname, int interprocess /* only with ZTS */);
+void xc_lock_destroy(xc_lock_t *lck);
+void xc_lock(xc_lock_t *lck);
+void xc_unlock(xc_lock_t *lck);
 
 #endif /* XC_LOCK_H_1913F3DED68715D7CDA5A055E79FE0FF */
